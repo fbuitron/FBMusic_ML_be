01bf1a5793e0e17e7b5ce3f96fa8af6deb04b59c
Franklin Buitron <franklin@Franklins-MacBook-Pro.local>
2016-11-21 19:46:46 -0600

    Fix the preprocessing needed for the whole data at once

--- RESTAPI/MachineLearning/Preprocessing.py
+++ RESTAPI/MachineLearning/Preprocessing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import neighbors
+from sklearn import preprocessing
 import MySQLdb
 import csv
 
@@ -22,8 +22,7 @@ def getDBConnection():
 def getRawData():
 	dbConn = getDBConnection()
 	cursor = dbConn.cursor()
-	
-	query = "SELECT af.trackID, af.acousticness,af.danceability, af.duration_ms, af.energy, af.instrumentalness, af.a_key, af.liveness, af.loudness, af.a_mode, af.speechiness, af.tempo, af.time_signature, af.valence, c.id  FROM AudioFeatures af INNER JOIN Track t ON t.ID = af.trackID INNER JOIN Playlist p ON p.id = t.playlistID INNER JOIN Category c ON p.categoryID = c.ID WHERE af.acousticness IS NOT NULL AND af.danceability IS NOT NULL AND af.duration_ms IS NOT NULL AND af.energy IS NOT NULL AND af.instrumentalness IS NOT NULL AND af.a_key IS NOT NULL AND af.liveness IS NOT NULL AND af.loudness IS NOT NULL AND af.a_mode IS NOT NULL AND  af.speechiness IS NOT NULL AND af.tempo IS NOT NULL AND af.time_signature IS NOT NULL AND af.valence AND c.ID in (SELECT cats.id from (SELECT c.id , count(t.id) from Track t INNER JOIN Playlist p ON p.ID = t.playlistID INNER JOIN Category c ON c.ID = p.categoryID  GROUP BY c.id) as cats) LIMIT 20"
+	query = "SELECT af.trackID, af.acousticness,af.danceability, af.duration_ms, af.energy, af.instrumentalness, af.a_key, af.liveness, af.loudness, af.a_mode, af.speechiness, af.tempo, af.time_signature, af.valence, c.id  FROM AudioFeatures af INNER JOIN Track t ON t.ID = af.trackID INNER JOIN Playlist p ON p.id = t.playlistID INNER JOIN Category c ON p.categoryID = c.ID WHERE af.acousticness IS NOT NULL AND af.danceability IS NOT NULL AND af.duration_ms IS NOT NULL AND af.energy IS NOT NULL AND af.instrumentalness IS NOT NULL AND af.a_key IS NOT NULL AND af.liveness IS NOT NULL AND af.loudness IS NOT NULL AND af.a_mode IS NOT NULL AND  af.speechiness IS NOT NULL AND af.tempo IS NOT NULL AND af.time_signature IS NOT NULL AND af.valence AND c.ID in (SELECT cats.id from (SELECT c.id , count(t.id) from Track t INNER JOIN Playlist p ON p.ID = t.playlistID INNER JOIN Category c ON c.ID = p.categoryID  GROUP BY c.id) as cats)"
 	cursor.execute(query)
 	results = cursor.fetchall()
 	return results
@@ -39,11 +38,24 @@ def writeCSV(listResults, filePath, titles=[]):
 titles = ['trackID', 'acousticness', 'danceability', 'duration_ms', 'energy', 'instrumentalness', 'a_key', 'liveness', 'loudness', 'a_mode', 'speechiness', 'tempo', 'time_signature', 'valence', 'category']
 # listResults = getRawData()
 # writeCSV(listResults, 'songs.csv', titles)
-# def testPandas():
-# 	df = pd.read_csv("songs.csv")
-# 	print(df.describe())
+def splitData():
+	df = pd.read_csv("songs.csv")
+	categories = df.category
+	trackIds = df.trackID
+	dataSet = df.ix[:,1:-1]
+	return dataSet, trackIds, categories
 
-# testPandas()
+def preprocess(dataSet):
+	# Key and Mode are the only data points that need to be treated differently in the preprocessing
+	df_dum = pd.get_dummies(dataSet,columns=['a_key', 'a_mode'])	
+	np_arr = np.array(df_dum)
+	min_max_scaler = preprocessing.MinMaxScaler()
+	normalized_data = min_max_scaler.fit_transform(np_arr)
+	print(normalized_data)
+	return normalized_data
+
+dataSet,trackIds,categories = splitData()
+preprocess(dataSet)
 
 
 
